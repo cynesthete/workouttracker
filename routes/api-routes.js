449ab60f4b2fa2bb35b6011bfae3c20a7eb81b1e
Tony Rivas <tonyjrivas@gmail.com>
2021-01-12 22:07:59 -0700

    fighting with mongoose

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,25 @@
 const router = require("express").Router();
-// change to require my workout model
-const Transaction = require("../models/transaction.js");
+const workout = require("../models/workout");
+
+//create route (no methods)
+router.route("/api/workouts")
+.get(function(req, res){
+    workout.find({})
+    .then(workout=>res.json(workout)).catch(err=>res.sendStatus(500))
+})
+
+//create workout 
+.post(function(req, res){
+    workout.create(req.body).then(workout=>res.json(workout)).catch(err=>res.sendStatus(500))
+})
+
+router.route("/api/workouts/:id")
+.put(function(req, res){
+    workout.findById(req.params.id)
+    .then(workout=>{
+        workout.exercises.push(req.body)
+        return workout.save()
+    }).then(workout=>res.json(workout)).catch(err=>res.sendStatus(500))
+})
+
+module.exports = router
